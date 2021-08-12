40d87c87aeb97bb8277bb4b213e539714feaec4c
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2021-08-12 21:47:31 +0800

    Top Scroll Button

--- js/custom.js
+++ js/custom.js
@@ -120,3 +120,15 @@ $(document).ready(function(){ //when document is ready
   }
   );
 });
+
+/*========== TOP SCROLL BUTTON ==========*/
+
+$(document).ready(function() { //when document is ready
+  $(window).scroll(function() { //when webpage is scrolled
+    if ($(this).scrollTop() > 500) { //if scroll from top is more than 500
+      $('.top-scroll').fadeIn(); //display element with class 'top-scroll'; opacity increases
+    } else { //if not
+      $('.top-scroll').fadeOut(); //hide element with class 'top-scroll'; opacity decreases
+    }
+  });
+});
